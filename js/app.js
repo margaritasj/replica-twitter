8cc74c3aba22679a5b4914c9725c0d5527e355a7
margb13 <marg.b13@hotmail.com>
2017-11-19 16:26:49 -0500

    Version 0.0.2

--- js/app.js
+++ js/app.js
@@ -1,15 +1,39 @@
 /* jshint browser: true */
 window.addEventListener('load', function(event) {
-var divText = document.getElementById('set-text');
-var btnTweet = document.getElementById('submit');
-var divTweet = document.getElementById('text-tweet');
-/*V 0.0.1 - evento click, obtener el texto y agregar el texto al HTML.*/  
-btnTweet.addEventListener('click', function(event) {
-  var cloneText = divText.cloneNode(true);
-  cloneText.removeAttribute('placeholder');
-  cloneText.removeAttribute('contenteditable');
-  cloneText.classList.remove('editable');
-  var tweet = divTweet.appendChild(cloneText);
-  divTweet.insertBefore(tweet,divTweet.childNodes[1]).classList.add('my-tweets');
+  var divText = document.getElementById('set-text');
+  var btnTweet = document.getElementById('submit');
+  var divTweet = document.getElementById('text-tweet');
+  var labelCount = document.getElementById('count');
+  /*V 0.0.1 - evento click, obtener el texto y agregar el texto al HTML.*/  
+  btnTweet.addEventListener('click', function(event) {
+    var cloneText = divText.cloneNode(true);
+    var tweet = divTweet.appendChild(cloneText); 
+    cloneText.removeAttribute('placeholder');
+    cloneText.removeAttribute('contenteditable');
+    cloneText.classList.remove('editable');
+    divTweet.insertBefore(tweet,divTweet.childNodes[1]).classList.add('my-tweets');
+    divText.innerHTML = '';
+    /*V 0.0.2 - No ingresar texto vacío (deshabilitar el botón de "twittear"),contador de caracteres de forma regresiva*/  
+    if (cloneText.innerHTML === '') {
+      divTweet.insertBefore(tweet,divTweet.childNodes[1]).classList.remove('my-tweets');
+    } else {
+      btnTweet.disabled = false;
+      btnTweet.classList.remove('disable-btn');
+      btnTweet.classList.add('enable-btn');
+      divTweet.insertBefore(tweet,divTweet.childNodes[1]).classList.add('my-tweets');
+      divText.innerHTML = '';
+    }
+  });
+  divText.addEventListener('keydown', function(event) {
+    var initial = '';
+    var caracteres = divText.innerHTML;
+    if (true) {
+      labelCount.innerHTML = caracteres.length + 1;
+    } else {
+      labelCount.innerHTML = caracteres.length - caracteres.length;  
+    }
+  });
+  
+  
 });
-});
+
