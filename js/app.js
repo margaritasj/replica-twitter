1045cabcf8e92020c3af7c92a315f9ab25b91b8d
margb13 <marg.b13@hotmail.com>
2017-11-19 19:06:53 -0500

    Version 0.0.3 - 1

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ window.addEventListener('load', function(event) {
   var btnTweet = document.getElementById('submit');
   var divTweet = document.getElementById('text-tweet');
   var labelCount = document.getElementById('count');
-  /*V 0.0.1 - evento click, obtener el texto y agregar el texto al HTML.*/  
+  /* V 0.0.1 - evento click, obtener el texto y agregar el texto al HTML.*/  
   btnTweet.addEventListener('click', function(event) {
     var cloneText = divText.cloneNode(true);
     var tweet = divTweet.appendChild(cloneText); 
@@ -13,27 +13,47 @@ window.addEventListener('load', function(event) {
     cloneText.classList.remove('editable');
     divTweet.insertBefore(tweet,divTweet.childNodes[1]).classList.add('my-tweets');
     divText.innerHTML = '';
-    /*V 0.0.2 - No ingresar texto vacío (deshabilitar el botón de "twittear"),contador de caracteres de forma regresiva*/  
+    /* V 0.0.2 - No ingresar texto vacío (deshabilitar el botón de "twittear"),contador de caracteres de forma regresiva*/  
     if (cloneText.innerHTML === '') {
       divTweet.insertBefore(tweet,divTweet.childNodes[1]).classList.remove('my-tweets');
     } else {
-      btnTweet.disabled = false;
-      btnTweet.classList.remove('disable-btn');
-      btnTweet.classList.add('enable-btn');
+      btnTweet.disabled = true;
+      btnTweet.classList.add('disable-btn');
+      btnTweet.classList.remove('enable-btn');
       divTweet.insertBefore(tweet,divTweet.childNodes[1]).classList.add('my-tweets');
       divText.innerHTML = '';
     }
   });
   divText.addEventListener('keydown', function(event) {
-    var initial = '';
     var caracteres = divText.innerHTML;
-    if (true) {
+    if (event) {
       labelCount.innerHTML = caracteres.length + 1;
+      btnTweet.disabled = true;
+      btnTweet.classList.remove('disable-btn');
+      btnTweet.classList.add('enable-btn');
+      if (caracteres.length <= 140 ) {
+        btnTweet.disabled = false;
+        labelCount.classList.add('black');
+      } else {
+        btnTweet.disabled = true;
+        btnTweet.classList.remove('enable-btn');
+        btnTweet.classList.add('disable-btn');
+      }
+      if (caracteres.length >= 121 && caracteres.length < 130 ) {
+        labelCount.classList.add('blue');
+      } else if (caracteres.length >= 131 && caracteres.length < 140) {
+        labelCount.classList.add('orange');
+      } else if (caracteres.length > 140) {
+        labelCount.classList.add('red');
+      } else {
+        labelCount.classList.remove('red');
+      }
     } else {
-      labelCount.innerHTML = caracteres.length - caracteres.length;  
+        labelCount.innerHTML = caracteres.length - caracteres.length;
     }
+
   });
-  
+
   
 });
 
